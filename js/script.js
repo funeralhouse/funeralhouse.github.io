1d8925d95a555da24040227a6dc000e9ec627388
funeralhouse <88926909+funeralhouse@users.noreply.github.com>
2021-08-27 19:49:41 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const iconMenu = document.querySelector('.menu_icon');
+if (iconMenu){
+  const menuBody = document.querySelector('.menu__body');
+  iconMenu.addEventListener("click", function (e) {
+iconMenu.classList.toggle('_active');
+menuBody.classList.toggle('_active');
+  })
+}
