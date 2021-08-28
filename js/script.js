e43dad5ad0838e6476af310a96bc0e0aca230b41
funeralhouse <88926909+funeralhouse@users.noreply.github.com>
2021-08-28 15:08:29 +0300

    Delete js directory

--- js/script.js
+++
@@ -1,8 +0,0 @@
-const iconMenu = document.querySelector('.menu_icon');
-if (iconMenu){
-  const menuBody = document.querySelector('.menu__body');
-  iconMenu.addEventListener("click", function (e) {
-iconMenu.classList.toggle('_active');
-menuBody.classList.toggle('_active');
-  })
-}
